8cd6d939c46d68042594226ad72a2c2ba2a39e37
M. Royhan Daffa <70930874+royhandf@users.noreply.github.com>
2022-01-19 06:19:10 +0700

    Membuat project kalkulator dengan java

---
+++ src/kalkulator/test.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package kalkulator;
+
+/**
+ *
+ * @author asus
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Test");
+        int a = 10;
+        int b = 3;
+        int hasil = a % b;
+        System.out.println("Hasil 10 % 3 = "+hasil);
+    }
+}
